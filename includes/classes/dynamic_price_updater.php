0bc8b2c3c7701c22fb4adcba697d90dd21e3a90a
Erik <info@zen4all.nl>
2013-05-06 09:23:39 +0200

    Multipple changes
    changed code to comply with Zen Cart standards

--- includes/classes/dynamic_price_updater.php
+++ includes/classes/dynamic_price_updater.php
@@ -4,215 +4,202 @@
 * (c) D Parry (Chrome) 2009 (admin@chrome.me.uk)
 * This module is released under the GNU/GPL licence... Really... Go look it up
 */
+if (!defined('IS_ADMIN_FLAG')) {
+  die('Illegal Access');
+}
+
 class DPU {
-	/**
-	* Current Zen Cart database object
-	*
-	* @var object
-	*/
-	var $_db;
-	/**
-	* Local copy of the POST array
-	*
-	* @var array
-	*/
-	var $_post = array();
-	/**
-	* Local instantiation of the shopping cart
-	*
-	* @var object
-	*/
-	var $_shoppingCart;
-	/**
-	* The type of message being sent (error or success)
-	*
-	* @var string
-	*/
-	var $_responseType = 'success';
-	/**
-	* Array of lines to be sent back.  The key of the array provides the attribute to identify it at the client side
-	* The array value is the text to be inserted into the node
-	*
-	* @var array
-	*/
-	var $_responseText = array();
+  
+  /**
+  * Local instantiation of the shopping cart
+  *
+  * @var object
+  */
+  var $_shoppingCart;
+  /**
+  * The type of message being sent (error or success)
+  *
+  * @var string
+  */
+  var $_responseType = 'success';
+  /**
+  * Array of lines to be sent back.  The key of the array provides the attribute to identify it at the client side
+  * The array value is the text to be inserted into the node
+  *
+  * @var array
+  */
+  var $_responseText = array();
 
-	/**
-	* Constructor
-	*
-	* @param obj The Zen Cart database class
-	* @return DPU
-	*/
-	function __construct(&$db) {
-		$this->_db =& $db;
-		$this->_post =& $_POST;
+  /**
+  * Constructor
+  *
+  * @param obj The Zen Cart database class
+  * @return DPU
+  */
+  function __construct() {
+    global $db;
+    // grab the shopping cart class and instantiate it
+    require_once(DIR_WS_CLASSES.'shopping_cart.php');
+    $this->_shoppingCart = new shoppingCart();
+  }
 
-		// grab the shopping cart class and instantiate it
-		require_once(DIR_WS_CLASSES.'shopping_cart.php');
-		$this->_shoppingCart = new shoppingCart();
-	}
+  /**
+  * PHP4 constructor
+  *
+  * @param obj ZC DB object
+  * @return DPU
+  */
+  function DPU() {
+    global $db;
+  }
+  /**
+  * Wrapper to call all methods to generate the output
+  *
+  * @return void
+  */
+  function getDetails() {
+    $this->insertProduct();
+    $this->_shoppingCart->calculate();
+    $show_dynamic_price_updater_sidebox = true;
+        if ($show_dynamic_price_updater_sidebox == true)   $this->getSideboxContent();
 
-	/**
-	* PHP4 constructor
-	*
-	* @param obj ZC DB object
-	* @return DPU
-	*/
-	function DPU(&$db) {
-		$this->__construct($db);
-	}
+    $this->prepareOutput();
+    $this->dumpOutput();
+  }
 
-	/**
-	* Wrapper to call all methods to generate the output
-	*
-	* @return void
-	*/
-	function getDetails() {
-		$this->insertProduct();
-		$this->_shoppingCart->calculate();
-        if (DPU_SHOW_SIDEBOX)   $this->getSideboxContent();
+  /**
+  * Wrapper to call all methods relating to returning multiple prices for category pages etc.
+  *
+  * @return void
+  */
+  function getMulti() {
+    $this->insertProducts();
+  }
 
-		$this->prepareOutput();
-		$this->dumpOutput();
-	}
+  /**
+  * Prepares the shoppingCart contents for transmission
+  *
+  * @return void
+  */
+  function prepareOutput() {
+    global $currencies,$db;
+    $this->_responseText['priceTotal'] = UPDATER_PREFIX_TEXT;
+    $product_check = $db->Execute("select products_tax_class_id from " . TABLE_PRODUCTS . " where products_id = '" . (int)$_POST['products_id'] . "'" . " limit 1");
+    if (false == DPU_SHOW_CURRENCY_SYMBOLS) {
+      $this->_responseText['priceTotal'] .= number_format($this->_shoppingCart->total, 2);
+    } else {
+      $this->_responseText['priceTotal'] .= $currencies->display_price($this->_shoppingCart->total, 0);
+    }
 
-	/**
-	* Wrapper to call all methods relating to returning multiple prices for category pages etc.
-	*
-	* @return void
-	*/
-	function getMulti() {
-		$this->insertProducts();
-	}
+    $this->_responseText['weight'] = (string)$this->_shoppingCart->weight;
+    if (DPU_SHOW_QUANTITY) {
+      $this->_responseText['quantity'] = sprintf(DPU_SHOW_QUANTITY_FRAME, $this->_shoppingCart->contents[$_POST['products_id']]['qty']);
+    }
+  }
 
-	/**
-	* Prepares the shoppingCart contents for transmission
-	*
-	* @return void
-	*/
-	function prepareOutput() {
-		global $currencies;
-		$this->_responseText['priceTotal'] = UPDATER_PREFIX_TEXT;
-		$product_check = $this->_db->Execute("select products_tax_class_id from " . TABLE_PRODUCTS . " where products_id = '" . (int)$this->_post['products_id'] . "'" . " limit 1");
-		if (false == DPU_SHOW_CURRENCY_SYMBOLS) {
-			$this->_responseText['priceTotal'] .= number_format($this->_shoppingCart->total, 2);
-		} else {
-			$this->_responseText['priceTotal'] .= $currencies->display_price($this->_shoppingCart->total, 0);
-		}
+  /**
+  * Inserts multiple non-attributed products into the shopping cart
+  *
+  * @return void
+  */
+  function insertProducts() {
+    foreach ($_POST['products_id'] as $id => $qty) {
+      $this->_shoppingCart->contents[] = array($id);
+      $this->_shoppingCart->contents[$id] = array('qty' => (float)$qty);
+    }
 
-		$this->_responseText['weight'] = (string)$this->_shoppingCart->weight;
-		if (DPU_SHOW_QUANTITY) {
-			$this->_responseText['quantity'] = sprintf(DPU_SHOW_QUANTITY_FRAME, $this->_shoppingCart->contents[$this->_post['products_id']]['qty']);
-		}
-	}
+    var_dump($this->_shoppingCart);
+    die();
+  }
 
-	/**
-	* Inserts multiple non-attributed products into the shopping cart
-	*
-	* @return void
-	*/
-	function insertProducts() {
-		foreach ($this->_post['products_id'] as $id => $qty) {
-			$this->_shoppingCart->contents[] = array($id);
-			$this->_shoppingCart->contents[$id] = array('qty' => (float)$qty);
-		}
+  /**
+  * Inserts the product into the shoppingCart content array
+  *
+  * @returns void
+  */
+  function insertProduct() {
+        $this->_shoppingCart->contents[$_POST['products_id']] = array('qty' => (float)$_POST['cart_quantity']);
+        $attributes = array();
 
-		var_dump($this->_shoppingCart);
-		die();
-	}
+        foreach ($_POST as $key => $val) {
+      if (is_array($val)) {
+        foreach ($val as $k => $v) {
+          $attributes[$k] = $v;
+        }
+      }
+        }
 
-	/**
-	* Inserts the product into the shoppingCart content array
-	*
-	* @returns void
-	*/
-	function insertProduct() {
-      	$this->_shoppingCart->contents[$this->_post['products_id']] = array('qty' => (float)$this->_post['cart_quantity']);
-      	$attributes = array();
+        if (is_array($attributes)) {
+          reset($attributes);
+          while (list($option, $value) = each($attributes)) {
+              //CLR 020606 check if input was from text box.  If so, store additional attribute information
+              //CLR 020708 check if text input is blank, if so do not add to attribute lists
+              //CLR 030228 add htmlspecialchars processing.  This handles quotes and other special chars in the user input.
+              $attr_value = NULL;
+              $blank_value = FALSE;
+              if (strstr($option, TEXT_PREFIX)) {
+                if (trim($value) == NULL) {
+                    $blank_value = TRUE;
+                } else {
+                    $option = substr($option, strlen(TEXT_PREFIX));
+              $attr_value = stripslashes($value);
+              $value = PRODUCTS_OPTIONS_VALUES_TEXT_ID;
+                $this->_shoppingCart->contents[$_POST['products_id']]['attributes_values'][$option] = $attr_value;
+                }
+              }
 
-      	foreach ($this->_post as $key => $val) {
-			if (is_array($val)) {
-				foreach ($val as $k => $v) {
-					$attributes[$k] = $v;
-				}
-			}
-      	}
+              if (!$blank_value) {
+                if (is_array($value) ) {
+                    reset($value);
+                    while (list($opt, $val) = each($value)) {
+                      $this->_shoppingCart->contents[$_POST['products_id']]['attributes'][$option.'_chk'.$val] = $val;
+                    }
+                } else {
+                    $this->_shoppingCart->contents[$_POST['products_id']]['attributes'][$option] = $value;
+                }
+              }
+          }
+      }
 
-      	if (is_array($attributes)) {
-        	reset($attributes);
-        	while (list($option, $value) = each($attributes)) {
-          		//CLR 020606 check if input was from text box.  If so, store additional attribute information
-          		//CLR 020708 check if text input is blank, if so do not add to attribute lists
-          		//CLR 030228 add htmlspecialchars processing.  This handles quotes and other special chars in the user input.
-          		$attr_value = NULL;
-          		$blank_value = FALSE;
-          		if (strstr($option, TEXT_PREFIX)) {
-            		if (trim($value) == NULL) {
-              			$blank_value = TRUE;
-            		} else {
-              			$option = substr($option, strlen(TEXT_PREFIX));
-				    	$attr_value = stripslashes($value);
-				    	$value = PRODUCTS_OPTIONS_VALUES_TEXT_ID;
-				      	$this->_shoppingCart->contents[$this->_post['products_id']]['attributes_values'][$option] = $attr_value;
-            		}
-          		}
-
-          		if (!$blank_value) {
-            		if (is_array($value) ) {
-              			reset($value);
-              			while (list($opt, $val) = each($value)) {
-                			$this->_shoppingCart->contents[$this->_post['products_id']]['attributes'][$option.'_chk'.$val] = $val;
-              			}
-            		} else {
-              			$this->_shoppingCart->contents[$this->_post['products_id']]['attributes'][$option] = $value;
-            		}
-          		}
-	        }
-	    }
-
-    	// $this->_shoppingCart->cleanup();
-	}
+      // $this->_shoppingCart->cleanup();
+  }
 
     /**
     * Prepares the output for the Updater's sidebox display
     *
     */
     function getSideboxContent() {
-        global $currencies;
+        global $currencies,$db;
 
-        $product_check = $this->_db->Execute("select products_tax_class_id from " . TABLE_PRODUCTS . " where products_id = '" . (int)$this->_post['products_id'] . "'" . " limit 1");
-        $product_query = "select products_id, products_price, products_tax_class_id, products_weight,
+        $product_check = $db->Execute("select products_tax_class_id from " . TABLE_PRODUCTS . " where products_id = '" . (int)$_POST['products_id'] . "'" . " limit 1");
+        $product = $db->Execute("select products_id, products_price, products_tax_class_id, products_weight,
                           products_priced_by_attribute, product_is_always_free_shipping, products_discount_type, products_discount_type_from,
                           products_virtual, products_model
                           from " . TABLE_PRODUCTS . "
-                          where products_id = '" . (int)$this->_post['products_id'] . "'";
+                          where products_id = '" . (int)$_POST['products_id'] . "'");
 
-        $product = $this->_db->Execute($product_query);
         $prid = $product->fields['products_id'];
         $products_tax = zen_get_tax_rate(0);
         $products_price = $product->fields['products_price'];
-        $qty = $this->_post['cart_quantity'];
+        $qty = $_POST['cart_quantity'];
         $out = array();
         $global_total;
-        reset($this->_shoppingCart->contents[$this->_post['products_id']]['attributes']);
-        while (list($option, $value) = each($this->_shoppingCart->contents[$this->_post['products_id']]['attributes'])) {
+        reset($this->_shoppingCart->contents[$_POST['products_id']]['attributes']);
+        while (list($option, $value) = each($this->_shoppingCart->contents[$_POST['products_id']]['attributes'])) {
             $adjust_downloads ++;
 
-            $attribute_price_query = "select *
+            $attribute_price = $db->Execute("select *
                                       from " . TABLE_PRODUCTS_ATTRIBUTES . "
                                       where products_id = '" . (int)$prid . "'
                                       and options_id = '" . (int)$option . "'
-                                      and options_values_id = '" . (int)$value . "'";
+                                      and options_values_id = '" . (int)$value . "'");
 
-            $attribute_price = $this->_db->Execute($attribute_price_query);
-
-            $sql = "SELECT
+            $data = $db->Execute("SELECT
                     `products_options_values_name`
                 FROM
                     ".TABLE_PRODUCTS_OPTIONS_VALUES."
                 WHERE
-                    `products_options_values_id` = $value";
-            $data = $this->_db->Execute($sql);
+                    `products_options_values_id` = $value");
             $name = $data->fields['products_options_values_name'];
 
             $new_attributes_price = 0;
@@ -255,50 +242,50 @@ class DPU {
                 }
             }
             $global_total += $total;
-            $qty2 = sprintf(DPU_SIDEBOX_QUANTITY_FRAME, $this->_post['cart_quantity']);
+            $qty2 = sprintf('<span class="DPUSideboxQuantity">'. DPU_SIDEBOX_QUANTITY_FRAME . '</span>', $_POST['cart_quantity']);
             $total = sprintf(DPU_SIDEBOX_PRICE_FRAME, $currencies->display_price($total, 0));
             $out[] = sprintf(DPU_SIDEBOX_FRAME, $name, $total, $qty2);
         }
 
-        $out[] = sprintf(DPU_SIDEBOX_TOTAL_FRAME, $currencies->display_price($this->_shoppingCart->total, 0));
+        $out[] = sprintf('<hr />' . DPU_SIDEBOX_TOTAL_FRAME, $currencies->display_price($this->_shoppingCart->total, 0));
 
-        $qty2 = sprintf(DPU_SIDEBOX_QUANTITY_FRAME, $this->_post['cart_quantity']);
+        $qty2 = sprintf('<span class="DPUSideboxQuantity">' . DPU_SIDEBOX_QUANTITY_FRAME . '</span>', $_POST['cart_quantity']);
         $total = sprintf(DPU_SIDEBOX_PRICE_FRAME, $currencies->display_price($this->_shoppingCart->total-$global_total, 0));
-        array_unshift($out, sprintf(DPU_SIDEBOX_FRAME, 'Base price', $total, $qty2));
+        array_unshift($out, sprintf(DPU_SIDEBOX_FRAME, DPU_BASE_PRICE, $total, $qty2));
 
         $this->_responseText['sideboxContent'] = implode('', $out);
     }
 
-	/**
-	* Performs an error dump
-	*
-	* @param mixed $errorMsg
-	*/
-	function throwError($errorMsg) {
-		$this->_responseType = 'error';
-		$this->_responseText[] = $errorMsg;
+  /**
+  * Performs an error dump
+  *
+  * @param mixed $errorMsg
+  */
+  function throwError($errorMsg) {
+    $this->_responseType = 'error';
+    $this->_responseText[] = $errorMsg;
 
-		$this->dumpOutput();
-	}
+    $this->dumpOutput();
+  }
 
-	/**
-	* Formats the response and flushes with the appropriate headers
-	* This should be called last as it issues an exit
-	*
-	* @return void
-	*/
-	function dumpOutput() {
-		// output the header for XML
-		header ("content-type: text/xml");
-		// set the XML file DOCTYPE
-		echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n";
-		// set the responseType
-		echo '<root>'."\n".'<responseType>'.$this->_responseType.'</responseType>'."\n";
-		// now loop through the responseText nodes
-		foreach ($this->_responseText as $key => $val) {
-			echo '<responseText'.(!is_numeric($key) && !empty($key) ? ' type="'.$key.'"' : '').'><![CDATA['.$val.']]></responseText>'."\n";
-		}
+  /**
+  * Formats the response and flushes with the appropriate headers
+  * This should be called last as it issues an exit
+  *
+  * @return void
+  */
+  function dumpOutput() {
+    // output the header for XML
+    header ("content-type: text/xml");
+    // set the XML file DOCTYPE
+    echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n";
+    // set the responseType
+    echo '<root>'."\n".'<responseType>'.$this->_responseType.'</responseType>'."\n";
+    // now loop through the responseText nodes
+    foreach ($this->_responseText as $key => $val) {
+      echo '<responseText'.(!is_numeric($key) && !empty($key) ? ' type="'.$key.'"' : '').'><![CDATA['.$val.']]></responseText>'."\n";
+    }
 
-		die('</root>');
-	}
+    die('</root>');
+  }
 }
