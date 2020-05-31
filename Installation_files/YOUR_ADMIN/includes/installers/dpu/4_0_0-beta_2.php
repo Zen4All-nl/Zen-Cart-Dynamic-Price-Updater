997756a7558c85dc6b0caa3f7560b91db5791d3a
Zen4All <info@zen4all.nl>
2020-05-31 07:53:07 +0200

    remove old beta version

--- Installation_files/YOUR_ADMIN/includes/installers/dpu/4_0_0-beta_2.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
-  V4.0.0, What changed:
-  - updated the jquery to 4.3.1
-  - rewritten the class and javascript. Removing all the ancient code, and moving to the builtin ajax functions of Zen Cart
-  - removed the Multi code formthe class, as it is not used (since v1). If neededit can always be put back in.
