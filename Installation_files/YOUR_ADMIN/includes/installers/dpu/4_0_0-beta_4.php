695672e408316afb71cd0078ca896c08355608b0
Erik Kerkhoven <info@zen4all.nl>
2020-12-20 11:30:29 +0100

    Update 4_0_0-beta_4.php

--- Installation_files/YOUR_ADMIN/includes/installers/dpu/4_0_0-beta_4.php
+++ Installation_files/YOUR_ADMIN/includes/installers/dpu/4_0_0-beta_4.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
   V4.0.0, What changed:
   - updated the jquery to 4.3.1
-  - rewrote the class and javascript, removing old code, and using the built-in ajax functions of Zen Cart
-  - removed the Multi code from the class as it is not used (since v1). If needed it can always be put back in.
+  - rewritten the class and javascript. Removing all the ancient code, and moving to the builtin ajax functions of Zen Cart
+  - removed the Multi code form the class, as it is not used (since v1). If neededit can always be put back in.
   - Added code changes from Torvista, MC12345678, and Zen4All
 
-*/
+ */
 $db->Execute("DELETE FROM " . TABLE_CONFIGURATION . " WHERE configuration_key = 'DPU_PLUGIN_CHECK'");
-
