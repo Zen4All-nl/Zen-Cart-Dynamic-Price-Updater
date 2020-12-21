8807d5346bafe6a8c60820fdf3152e084029509d
Steve <4391026+torvista@users.noreply.github.com>
2020-12-21 09:47:32 +0100

    modify uninstall so it hinges on constant rather than text

--- uninstall_dpu.sql
+++ uninstall_dpu.sql
@@ -1,11 +1,6 @@
-/* Tables need to be ensured to include the prefix if it exists. */
+/* Paste and run this sql in the Zen Cart Admin->SQL patch Tool AFTER removing all the DPU files */
 
-
-SELECT @DPUgID := configuration_group_id 
-FROM configuration_group where configuration_group_title = 'Dynamic Price Updater Config';
-
+SELECT @DPUgID := configuration_group_id FROM configuration WHERE configuration_key = 'DPU_VERSION';
 DELETE FROM configuration WHERE configuration_group_id = @DPUgID; 
-
 DELETE FROM admin_pages WHERE page_key = 'configDynamicPriceUpdater';
-
 DELETE FROM configuration_group WHERE configuration_group_id = @DPUgID;
