c8a5dd585063d12933919a68b7d17b3f6f13ee03
John <dbltoe@users.noreply.github.com>
2023-02-16 10:45:47 -0600

    Update 4_0_0-beta_3.php

--- Installation_files/YOUR_ADMIN/includes/installers/dpu/4_0_0-beta_3.php
+++ Installation_files/YOUR_ADMIN/includes/installers/dpu/4_0_0-beta_3.php
@@ -1,7 +1,3 @@
 <?php
 /**
-  V4.0.0, What changed:
-  - updated the jquery to 4.3.1
-  - rewritten the class and javascript. Removing all the ancient code, and moving to the builtin ajax functions of Zen Cart
-  - removed the Multi code form the class, as it is not used (since v1). If neededit can always be put back in.
-  - Added code changes from Torvista, MC12345678, and Zen4All
+  V5.0.0, What changed:
