1135a48029d678f2d457f138eca5457b7e61c89c
John <dbltoe@users.noreply.github.com>
2023-02-16 10:35:56 -0600

    Update tpl_dynamic_price_updater_sidebox.php

--- Installation_files/includes/templates/YOUR_TEMPLATE/sideboxes/tpl_dynamic_price_updater_sidebox.php
+++ Installation_files/includes/templates/YOUR_TEMPLATE/sideboxes/tpl_dynamic_price_updater_sidebox.php
@@ -4,7 +4,7 @@
  *
  * @package templateSystem
  * @copyright 2007 Kuroi Web Design
-  * @copyright Portions Copyright 2003-2007 Zen Cart Development Team
+ * @copyright Portions Copyright 2003-2007 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  */
 
