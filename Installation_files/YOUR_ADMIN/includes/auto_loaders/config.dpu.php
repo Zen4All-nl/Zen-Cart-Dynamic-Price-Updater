d18d0dcb604d16ce4277affe264c51d20d873338
Steve <4391026+torvista@users.noreply.github.com>
2023-03-07 10:29:13 +0100

    short array syntax, whitespace, format

--- Installation_files/YOUR_ADMIN/includes/auto_loaders/config.dpu.php
+++ Installation_files/YOUR_ADMIN/includes/auto_loaders/config.dpu.php
@@ -2,17 +2,17 @@
 // Dynamic Price Updater 5.0
 
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
-} 
+    die('Illegal Access');
+}
 
 if (IS_ADMIN_FLAG === true) {
-  $autoLoadConfig[999][] = array(
-    'autoType' => 'init_script',
-    'loadFile' => 'init_dpu_config.php'
-  );
+    $autoLoadConfig[999][] = [
+        'autoType' => 'init_script',
+        'loadFile' => 'init_dpu_config.php'
+    ];
 } else {
-  trigger_error(__FILE__ . ' loaded from catalog side, verify upload of files.', E_USER_WARNING);
-  @unlink(__FILE__);
+    trigger_error(__FILE__ . ' loaded from catalog side, verify upload of files.', E_USER_WARNING);
+    @unlink(__FILE__);
 }
 
 // uncomment the following line to perform a uninstall
