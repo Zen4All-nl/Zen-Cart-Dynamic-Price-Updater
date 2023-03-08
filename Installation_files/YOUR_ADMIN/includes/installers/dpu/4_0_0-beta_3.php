225e00a9dde7622df2728e171ea230b0e87b9993
Steve <4391026+torvista@users.noreply.github.com>
2023-03-08 10:51:24 +0100

    fix unterminated comment

--- Installation_files/YOUR_ADMIN/includes/installers/dpu/4_0_0-beta_3.php
+++ Installation_files/YOUR_ADMIN/includes/installers/dpu/4_0_0-beta_3.php
@@ -1,7 +1,8 @@
 <?php
 /**
   V4.0.0, What changed:
-  - updated the jquery to 4.3.1
-  - rewritten the class and javascript. Removing all the ancient code, and moving to the builtin ajax functions of Zen Cart
-  - removed the Multi code form the class, as it is not used (since v1). If neededit can always be put back in.
-  - Added code changes from Torvista, MC12345678, and Zen4All
+  - updated jquery to 4.3.1
+  - rewrote the class and javascript, removing all the ancient code, and moving to the built-in ajax functions of Zen Cart
+  - removed the Multi code form the class, as it was not used (since v1).
+  - Added code changes from torvista, mc12345678, and Zen4All
+**/
