30a46ee8675b7af81d1ca6a8aff4c0c14dca54c5
Steve <4391026+torvista@users.noreply.github.com>
2023-03-11 13:13:54 +0100

    comments, type hints, consolidate case

--- Installation_files/includes/classes/dynamic_price_updater.php
+++ Installation_files/includes/classes/dynamic_price_updater.php
@@ -23,36 +23,33 @@ class DPU extends base
      * @return array
      * @global object $db
      */
-    public function getOptionPricedIds($products_id)
+    public function getOptionPricedIds(int $products_id): array
     {
         global $db;
-        // Identify the attribute information associated with the provided $products_id.
         $attribute_price_query = 'SELECT *
-                              FROM ' . TABLE_PRODUCTS_ATTRIBUTES . '
-                              WHERE products_id = ' . (int)$products_id . '
-                              ORDER BY options_id, options_values_price';
-
+                                  FROM ' . TABLE_PRODUCTS_ATTRIBUTES . '
+                                  WHERE products_id = ' . $products_id . '
+                                  ORDER BY options_id, options_values_price';
         $attribute_price = $db->Execute($attribute_price_query);
 
         $last_id = 'X';
         $options_id = [];
 
-        // Populate $options_id to contain the options_ids that potentially affect price.
+        //populate array $options_id with only the options_ids that may modify the price
         while (!$attribute_price->EOF) {
-            // Basically if the options_id has already been captured, then don't try to process again.
+
+            // skip if this options_id has already been captured
             if ($last_id == $attribute_price->fields['options_id']) {
                 $attribute_price->MoveNext();
                 continue;
             }
 
-            /* Capture the options_id of option names that could affect price
-
-              Identify an option name that could affect price by:
+            /* Identify an option name that could affect price by:
               having a price that is not zero,
               having quantity prices (though this is not (yet) deconstruct the prices and existing quantity),
               having a price factor that could affect the price,
               is a text field that has a word or letter price.
-             */
+            */
             if (!(
                     $attribute_price->fields['options_values_price'] == 0 &&
                     !zen_not_null($attribute_price->fields['attributes_qty_prices']) &&
@@ -74,14 +71,11 @@ class DPU extends base
                 )
             ) {
                 $prefix_format = 'id[:option_id:]';
-
                 $attribute_type = zen_get_attributes_type($attribute_price->fields['products_attributes_id']);
 
                 switch ($attribute_type) {
-                    case (PRODUCTS_OPTIONS_TYPE_TEXT):
-                        $prefix_format = $db->bindVars($prefix_format, ':option_id:', TEXT_PREFIX . ':option_id:', 'noquotestring');
-                        break;
                     case (PRODUCTS_OPTIONS_TYPE_FILE):
+                    case (PRODUCTS_OPTIONS_TYPE_TEXT):
                         $prefix_format = $db->bindVars($prefix_format, ':option_id:', TEXT_PREFIX . ':option_id:', 'noquotestring');
                         break;
                     default:
@@ -95,11 +89,8 @@ class DPU extends base
                 $attribute_price->MoveNext();
                 continue;
             }
-
             $attribute_price->MoveNext();
         }
-
         return $options_id;
     }
-
 }
